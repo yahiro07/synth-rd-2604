a181cf6de8d9e4461d32a118b82e4403cdcc12ab
yahiro <yahiro07@users.noreply.github.com>
2026-05-25 12:46:47 +0900

    change module name

---
+++ sr2605/synth-lib/packages/note-voicing-adapter/note-voicing-duration-adapter.ts
@@ -0,0 +1,62 @@
+export function createNoteVoicingDurationAdapter(destNotePort: {
+  noteOn(noteNumber: number): void;
+  noteOff(noteNumber: number): void;
+}) {
+  // console.log("noteVoicingAdapter 0907");
+
+  const sentNotes = new Set<number>();
+  const offTimers = new Map<number, ReturnType<typeof setTimeout>>();
+
+  const internal = {
+    emitNoteOn(noterNumber: number) {
+      destNotePort.noteOn(noterNumber);
+      sentNotes.add(noterNumber);
+    },
+    emitNoteOff(noteNumber: number) {
+      if (sentNotes.has(noteNumber)) {
+        destNotePort.noteOff(noteNumber);
+        sentNotes.delete(noteNumber);
+      }
+    },
+    reserveNoteOff(noteNumber: number, durationSec: number) {
+      const timer = setTimeout(() => {
+        offTimers.delete(noteNumber);
+        internal.emitNoteOff(noteNumber);
+      }, durationSec * 1000);
+      offTimers.set(noteNumber, timer);
+    },
+    cancelNoteOffReservation(noteNumber: number) {
+      const timer = offTimers.get(noteNumber);
+      if (timer !== undefined) {
+        clearTimeout(timer);
+        offTimers.delete(noteNumber);
+      }
+    },
+    allNotesOff() {
+      for (const timer of offTimers.values()) {
+        clearTimeout(timer);
+      }
+      offTimers.clear();
+      for (const note of sentNotes) {
+        destNotePort.noteOff(note);
+      }
+      sentNotes.clear();
+    },
+  };
+  return {
+    noteOn(noteNumber: number, durationSec?: number) {
+      internal.cancelNoteOffReservation(noteNumber);
+      internal.emitNoteOn(noteNumber);
+      if (durationSec !== undefined) {
+        internal.reserveNoteOff(noteNumber, durationSec);
+      }
+    },
+    noteOff(noteNumber: number) {
+      internal.cancelNoteOffReservation(noteNumber);
+      internal.emitNoteOff(noteNumber);
+    },
+    allNotesOff() {
+      internal.allNotesOff();
+    },
+  };
+}
