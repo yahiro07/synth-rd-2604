db13aa7696fa88eff841c89eb5f1fe30eed89e43
yahiro <yahiro07@users.noreply.github.com>
2026-06-01 14:55:35 +0900

    create project

---
+++ sr2606/jam-desk-d2/src/index.tsx
@@ -0,0 +1,10 @@
+import "./page.css";
+import "beams/ax-ui/utility-classes.css";
+//
+import { mountAppRoot } from "beams/ax-react/mount-app-root";
+
+const App = () => {
+  return <div>hello</div>;
+};
+
+mountAppRoot(<App />);
